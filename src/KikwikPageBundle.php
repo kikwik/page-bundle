97d6913ad2f45e5f160c5ef04f52e687add9408a
Klaus <support@kikwik.it>
2024-08-15 14:13:41 +0200

    require symfony-cmf/routing-bundle and stof/doctrine-extensions-bundle

--- src/KikwikPageBundle.php
+++ src/KikwikPageBundle.php
@@ -3,9 +3,21 @@
 
 namespace Kikwik\PageBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class KikwikPageBundle extends AbstractBundle
 {
 
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/packages/cmf_routing.yaml');
+        $container->import('../config/packages/stof_doctrine_extensions.yaml');
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.xml');
+    }
 }
