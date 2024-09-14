2effa841b90943e29455816f10c35811dce48bc7
Klaus <support@kikwik.it>
2024-09-14 17:40:11 +0200

    firstItemLabel in Breadcrumbs component

--- src/Twig/Components/Breadcrumbs.php
+++ src/Twig/Components/Breadcrumbs.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
 class Breadcrumbs
 {
+    public ?string $firstItemLabel = null;
+
     public array $extraLink = [];
 
     public function __construct(
@@ -26,12 +28,19 @@ class Breadcrumbs
         $pageTranslation = $this->getPageTranslation();
         if ($pageTranslation)
         {
+            // Costruisce il path all'indietro
             array_unshift($result, [$pageTranslation->getUrl() => $pageTranslation->getTitle()]);
             while ($pageTranslation->getParent())
             {
                 $pageTranslation = $pageTranslation->getParent();
                 array_unshift($result, [$pageTranslation->getUrl() => $pageTranslation->getTitle()]);
             }
+        }
+        if(count($result) && $this->firstItemLabel)
+        {
+            // Sovrascrive la label del primo elemento
+            $firstKey = array_key_first($result[0]);
+            $result[0][$firstKey] = $this->firstItemLabel;
         }
 
         // Flat the array to get key-value pairs
