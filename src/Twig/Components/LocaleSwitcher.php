cb88a6b3f7b2ece3dc2da5f36d24dcd1b5a139d0
Klaus <support@kikwik.it>
2025-05-16 09:51:14 +0200

    fix LocaleSwitcher

--- src/Twig/Components/LocaleSwitcher.php
+++ src/Twig/Components/LocaleSwitcher.php
@@ -38,18 +38,29 @@ class LocaleSwitcher
         $result = [];
         if($this->getPageTranslation())
         {
+            $tmpResult = [];
+            // list translations from current page (sblings of $this->getPageTranslation())
             $page = $this->getPageTranslation()->getPage();
             /** @var PageTranslationInterface $translation */
             foreach($page->getTranslations() as $translation)
             {
-                if($translation->getId() && $translation->isEnabled())
+                if($translation->getId() && $translation->isEnabled() && in_array($translation->getLocale(), $this->enabledLocales))
                 {
-                    $result[$translation->getLocale()] = $translation->getUrl().$this->getExtraSlug();
+                    $tmpResult[$translation->getLocale()] = $translation->getUrl().$this->getExtraSlug();
+                }
+            }
+            // sort links by the $this->enabledLocales order
+            foreach($this->enabledLocales as $locale)
+            {
+                if(isset($tmpResult[$locale]))
+                {
+                    $result[$locale] = $tmpResult[$locale];
                 }
             }
         }
         else
         {
+            // list translations from $this->enabledLocales
             $route = $this->requestStack->getCurrentRequest()->get('_route');
             if($route)
             {
